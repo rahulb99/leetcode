792ce1f27375e347973240fce941347e31c731f1
Rahul Baid <rahulbaid0110@gmail.com>
2025-04-03 09:18:55 -0500

    update recent to only faang

--- recent.py
+++ recent.py
@@ -3,14 +3,19 @@ import pandas as pd
 
 # Define the filename to search for in subfolders
 target_filename = "1. Thirty Days.csv"
-output_filename = "Most Frequent.csv"
+output_filename = "recent.csv"
 
 # List to store dataframes
 dataframes = []
 
+# Top tech companies
+companies = ["Meta", "Apple", "Amazon", "Google", "Microsoft", "Netflix", "Nvidia"]
+
 # Walk through all subdirectories
 for root, dirs, files in os.walk("."):
-    if target_filename in files:
+    # Check if the current directory name is in the list of companies
+    # Check if the target filename exists in the current directory
+    if any(company in root for company in companies) and target_filename in files:
         file_path = os.path.join(root, target_filename)
         # Read the CSV file and append it to the list
         try:
